0e88f157280970038f28bd013fd76dd0adfcede1
sppiyush <piyushpkcorp@gmail.com>
2024-02-12 13:17:26 +0530

    Intro.txt and Largest.java created

---
+++ Largest.java
@@ -0,0 +1,30 @@
+// Q - Take three integers as an input from the user and find the largest among them 
+
+import java.util.Scanner;
+
+public class Largest
+{
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the three numbers :- ");
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int max = a ;
+        if(b>max)
+        {
+            max = b;
+        }
+
+        if(c > max)
+        {
+            max = c;
+        }
+
+        System.out.println(max);
+    }
+}
