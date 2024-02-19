019a095d959d58d2e5461394c157af8380725ad9
sppiyush <piyushpkcorp@gmail.com>
2024-02-19 13:36:53 +0530

    modified

--- Ceiling.java
+++ Ceiling.java
@@ -3,6 +3,11 @@ public class Ceiling
 {
     static int ceiling(int[] arr,int target)
     {
+        // but what if the target is greater than the greatest number in the array 
+        if(target > arr[arr.length - 1])
+        {
+            return -1;
+        }
         int start = 0;
         int end = arr.length - 1;
 
@@ -52,7 +57,7 @@ public class Ceiling
     public static void main(String[] args)
     {
         int[] arr = {23,45,89,100,102,120,123};
-        int target = 24;
+        int target = 46;
         int ans = ceiling(arr,target);
         System.out.println(ans);
     }
