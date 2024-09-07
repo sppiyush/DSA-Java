0e54ba3c292c2294e571020422d66558547e66a5
sppiyush <piyushpkcorp@gmail.com>
2024-09-07 16:19:04 +0530

    Update FirstDig.java

--- FirstDig.java
+++ FirstDig.java
@@ -34,3 +34,33 @@ Thus, the time complexity is O(log₁₀(n)), where n is the input number.
 Space Complexity:
 The space complexity is O(1) since no additional space is required except for a few variables used in the method.
  */
+
+
+
+/*
+ * import java.util.Scanner;
+
+public class FirstNum{
+    public static void main(String[] args)
+    {
+        System.out.print("Enter the number :- ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.print(FirstDigit(n));
+    }
+    
+    public static int FirstDigit(int n)
+    {
+        double power = Math.log10(n);
+        int p = (int) power;
+        int a = (int) Math.pow(10,p);
+        int ans = n / a;
+        return ans;
+    }
+}
+
+
+The time complexity of the given Java code is O(1) (constant time). Since all the operations in the code are performed in constant time, the overall time complexity is O(1).
+
+The space complexity is also O(1) since the program only uses a fixed amount of extra space (variables power, p, a, and ans), regardless of the size of the input.
+ */
